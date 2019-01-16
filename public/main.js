bc44db01853f633818c25fd56ee3c368955f2764
Cindy Knottnerus <cknottne@gmail.com>
2019-01-16 17:10:17 +0100

    calendar

--- public/main.js
+++ public/main.js
@@ -10,21 +10,24 @@ var app = new Vue({
 		messages: [],
 		currentView: "index",
 		seen: false,
+		text: {}
 	},
+
 	created() {
-		this.getActivitiesData();
+//		this.getActivitiesData();
 		this.checkIfUserLoggedIn();
 	},
 	methods: {
 		getActivitiesData() {
 
 			fetch(this.url, {
-					method: "GET",
+					method: "GET"
 				})
 				.then(r => r.json())
 				.then(json => {
 					app.data = json;
 					app.getPosts();
+					app.myFunction();
 					console.log(app.data);
 
 				})
@@ -45,15 +48,22 @@ var app = new Vue({
 				return
 			}
 			const text = document.getElementById("textInput").value;
+
+			var reference = app.findHashtags(text);
+
 			const userName = firebase.auth().currentUser.displayName;
 			const email = firebase.auth().currentUser.email;
 			const photoURL = firebase.auth().currentUser.photoURL;
+
 			var post = {
 				name: userName,
 				body: text,
 				email: email,
 				image: photoURL,
+				result: text,
 			};
+
+			console.log(post)
 
 			const newPostKey = firebase.database().ref().child('chat').push().key;
 
@@ -64,8 +74,22 @@ var app = new Vue({
 			return firebase.database().ref().update(updates);
 			app.getPosts();
 		},
+
+		findHashtags(searchtextInput) {
+			var regexp = /\B\#\w\w+\b/g
+			result = searchtextInput.match(regexp);
+			if (result) {
+				return result;
+			} else {
+				return false;
+			}
+		},
+
+
 		getPosts: function () {
+			app.filterTheHashtagMessages("#Blah");
 			firebase.database().ref('chat').on('value', function (data) {
+
 
 				var userEmail = firebase.auth().currentUser.email;
 
@@ -78,15 +102,22 @@ var app = new Vue({
 				for (var key in messages) {
 					let element = messages[key];
 
+					var b = app.hashtag(element.body);
+					if (b == '<a @click=filterTheHashtagMessages("Blah")>#happy</a>') {
+						element["hashtag"] = true
+					}
+
 					if (element.email == userEmail) {
 						element["user"] = "hostpost";
 					} else {
 						element["user"] = "otherpost";
 					}
-					allPosts.push(element);
 
+					allPosts.push(element);
+					console.log(element)
 				}
 				app.messages = allPosts;
+				console.log(app.messages)
 
 				setTimeout(function () {
 					$("#chat-window").animate({
@@ -103,12 +134,14 @@ var app = new Vue({
 			app.userLogged = false;
 		},
 		setcurrentView(view) {
-			console.log(view)
+			//					console.log(view)
 			this.currentView = view;
 			if (this.currentView == "general") {
 				app.seen = true;
 				app.getPosts();
 			}
+			if (this.currentView == 'activities')
+				this.getActivitiesData()
 
 		},
 		checkIfUserLoggedIn() {
@@ -119,7 +152,43 @@ var app = new Vue({
 					app.userLogged = false;
 				}
 			});
+		},
+		filterTheHashtagMessages(hashtag) {
+			firebase.database().ref('chat').on('value', function (data) {
+				var messageObject = data.val();
+				var emptyObject = [];
+				for (key in messageObject) {
+					var array = messageObject[key].result;
+					if (array) {
+						for (var i = 0; i < array.length; i++) {
+							if (array[i] == hashtag) {
+								emptyObject.push(messageObject[key])
+							}
+						}
+					}
+				}
+				console.log("The array of objects:", emptyObject)
+			})
+		},
+		hashtag(text) {
+
+			var repl = text.replace(/#(\w+)/g, '<a @click=filterTheHashtagMessages("Blah")>#$1</a>');
+			return repl;
+			//					var linkingTheText = document.getElementById("text");
+			//					linkingTheText.innerHTML = repl;
+		},
+
+
+		myFunction() {
+			$('#example3').calendar({
+				type: 'date'
+			});
 		}
+
+
+
+
+
+
 	}
 });
-
